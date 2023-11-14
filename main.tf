a3e04ca90ec9982fd5edc7267c251e1faff729b8
slumlord <jbouey2006@gmail.com>
2023-11-14 16:28:49 -0500

    fixes2

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ module "vpc" {
   region                 = local.region
   project_name           = local.project_name
   environment            = local.environment
-  vpc_cidr               = var.vpc.vpc_cidr
+  vpc_cidr               = var.vpc_cidr
   public_subnet_az1_cidr = var.public_subnet_az1_cidr
   public_subnet_az2_cidr = var.public_subnet_az2_cidr
   private_app_az1_cidr   = var.private_app_az1_cidr
